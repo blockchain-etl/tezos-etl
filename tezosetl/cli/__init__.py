cb1a12fab4db5097899835015d532da00616a772
medvedev1088 <medvedev1088@gmail.com>
2020-06-26 16:55:47 +0700

    Bump version

--- tezosetl/cli/__init__.py
+++ tezosetl/cli/__init__.py
@@ -27,7 +27,7 @@ from tezosetl.cli.get_block_range_for_date import get_block_range_for_date
 
 
 @click.group()
-@click.version_option(version='1.2.0')
+@click.version_option(version='1.2.1')
 @click.pass_context
 def cli(ctx):
     pass
